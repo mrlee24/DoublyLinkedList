212acb26d18f041c74841114765e7b8d765d1040
Long Ly <lymanhlong@gmail.com>
2020-04-29 13:23:50 -0400

    Initial Commit

---
+++ FinalExam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalExam
+//
+//  Created by Long Ly on 2020-04-29.
+//  Copyright © 2020 Long Ly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
